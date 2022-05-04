c1d5d8f2702a736e8311fdf7e0f771a2f56bdf78
m-veloper <m_veloper@naver.com>
2022-05-05 01:32:29 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/covisiontask/CovisionTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.covisiontask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovisionTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovisionTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/covisiontask/CovisionTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.covisiontask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovisionTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
